7c1311f084ef65c5534c1920cc1766bdfc4843f8
hiro0214 <leonhurt2@gmail.com>
2022-03-12 05:19:18 +0900

    add [palette, font]

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,4 +1,8 @@
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
-  customSyntax: 'postcss-scss',
+  extends: ["stylelint-config-standard", "stylelint-config-recess-order"],
+  customSyntax: "postcss-scss",
+  rules: {
+    "at-rule-no-unknown": null,
+    "function-no-unknown": null,
+  },
 };
